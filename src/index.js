c4b785761322a97b8ed827f3564d0de742e13744
Bima sandika <bimasandika@gmail.com>
2024-05-05 11:09:05 -0700

    swagger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-import { getAll, getById, create, update, remove } from "./user.js";
+import { getAll, getById, create, update, remove } from "./user.js"
 
 const app = express();
 const port = 3000;
@@ -9,15 +9,15 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Definisi konfigurasi Swagger
 const options = {
   definition: {
     openapi: "3.0.0",
     info: {
       title: "User API",
       version: "1.0",
-      description: "OpenAPI for Users"
+      description : "OpenAPI for Users"
     },
+
     components: {
       securitySchemes: {
         access_token: {
@@ -36,17 +36,172 @@ const options = {
   apis: ["./src/*.js"],
 };
 
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 const swaggerDoc = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc, {customCssUrl : CSS_URL}));
 
-// Gunakan middleware Swagger UI untuk menampilkan dokumentasi API
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      required:
+ *        - name
+ *        - email
+ *      properties:
+ *        id:
+ *          type: integer
+ *          description: The auto-generated id of the user
+ *        name:
+ *          type: string
+ *          description: The name of the user
+ *        email:
+ *          type: string
+ *          format: email
+ *          description: The email of the user
+ *      example:
+ *        id: 3
+ *        name: user3
+ *        email: user@gmail.com
+ * 
+ *    
+ */
 
-// Daftarkan endpoint-endpoint API
+/**
+ * @swagger
+ * /users:
+ *    get:
+ *      summary: Get all users
+ *      tags: [Users]
+ *      responses:
+ *        200:
+ *          description: The list of all users
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/User'
+ */
+
+
 app.get("/users", getAll);
+
+
+
+/**
+ * @swagger
+ * /users/{id}:
+ *    get:
+ *      summary: Get user by id
+ *      tags: [Users]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *            type: integer
+ *          required: true
+ *          description: The user id
+ *      responses:
+ *        '200':
+ *          description: The user description by id
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  $ref: '#/components/schemas/User'
+ *        '404':
+ *          description: User not found
+ */
+
 app.get("/users/:id", getById);
-app.post('/users', create);
-app.put('/users/:id', update);
-app.delete('/users/:id', remove);
+
+
+/**
+ * @swagger
+ * /users:
+ *    post:
+ *      summary: Create a new user
+ *      tags: [Users]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      responses:
+ *        '200':
+ *          description: The user was successfully created
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  $ref: '#/components/schemas/User'
+ *        '400':
+ *          description: Invalid user data
+ */
+
+app.post('/users', create)
+
+
+/**
+ * @swagger
+ * /users/{id}:
+ *  put:
+ *    summary: Update the user by the id
+ *    tags: [Users]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: the user id
+ *    requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: the user was updated
+ *        content: 
+ *          application/json:
+ *            schema:
+ *             $ref: '#/components/schemas/User'
+ *      404:
+ *        description: The user was not found
+ */
+
+
+app.put('/users/:id', update)
+
+
+/**
+ * @swagger
+ * /users/{id}:
+ *    delete:
+ *      summary: Remove the user by id
+ *      tags: [Users]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *            type: integer
+ *          required: true
+ *          description: The user id
+ *      responses:
+ *        '200':
+ *          description: the user was deleted
+ *        '404':
+ *          description: the was user not found
+ */
+
+
+app.delete('/users/:id', remove)
+
 
 app.get("/", (req, res) => {
   res.send("halo world");
